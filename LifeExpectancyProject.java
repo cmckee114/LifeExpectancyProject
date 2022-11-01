4835b6323679583828d4130e6a058bc154d622d1
Your Name <cassidyrosemckee@gmail.com>
2022-11-01 19:47:44 -0400

    coded the portion of my code where I access the data in my dataset

--- LifeExpectancyProject.java
+++ LifeExpectancyProject.java
@@ -18,88 +18,119 @@ public class LifeExpectancyProject{
         // System.out.println(incomeQuartileDifference(lifeExpectancyPathname));
     }
 
-//     public static void incomeQuartileDifference (String lifeExpectancyPathname) throws FileNotFoundException{
-//         File lifeExpectancyFile = new File (lifeExpectancyPathname);
-//         Scanner scanner = new Scanner(lifeExpectancyFile);
+    public static void incomeQuartileDifference (String lifeExpectancyPathname) throws FileNotFoundException{
+        File lifeExpectancyFile = new File (lifeExpectancyPathname);
+        Scanner scanner = new Scanner(lifeExpectancyFile);
 
-//         //creating an array for the header of the dataset
-//         String[] header = sc.nextLine().split(",");
-//         //finding the index of the columns that have the dadta I want to use 
-//         int quartile1MIndex = header.indexOf("le_agg_q1_M");
-//         int quartile1FIndex = header.indexOf("le_agg_q1_F");
-//         int quartile2MIndex = header.indexOf("le_agg_q2_M");
-//         int quartile2FIndex = header.indexOf("le_agg_q2_F");
-//         int quartile3MIndex = header.indexOf("le_agg_q3_M");
-//         int quartile3FIndex = header.indexOf("le_agg_q3_F");
-//         int quartile4MIndex = header.indexOf("le_agg_q4_M");
-//         int quartile4FIndex = header.indexOf("le_agg_q4_F");
+        //creating an array for the header of the dataset
+        String[] header = sc.nextLine().split(",");
+        //finding the index of the columns that have the dadta I want to use 
+        int quartile1MIndex = header.indexOf("le_agg_q1_M");
+        int quartile1FIndex = header.indexOf("le_agg_q1_F");
+        int quartile2MIndex = header.indexOf("le_agg_q2_M");
+        int quartile2FIndex = header.indexOf("le_agg_q2_F");
+        int quartile3MIndex = header.indexOf("le_agg_q3_M");
+        int quartile3FIndex = header.indexOf("le_agg_q3_F");
+        int quartile4MIndex = header.indexOf("le_agg_q4_M");
+        int quartile4FIndex = header.indexOf("le_agg_q4_F");
         
-//         //setting up the array that will hold my final data with the quartile average life Expectancies inside 
-//         double[] quartileAverages = {0,0,0,0};
+        //setting up the array that will hold my final data with the quartile average life Expectancies inside 
+        Double[] quartileAveragesAsArr = {0,0,0,0};
+        ArrayList<Double> quartileAverages = new ArrayList<>(Arrays.asList(quartileAveragesAsArr));
+        System.out.println(quartileAverages);
 
-//         //each list holds the average of F and M life expectancy per state for that quartile 
-//         ArrayList<Double> Q1 = new ArrayList<Double>();
-//         ArrayList<Double> Q2 = new ArrayList<Double>();
-//         ArrayList<Double> Q3 = new ArrayList<Double>();
-//         ArrayList<Double> Q4 = new ArrayList<Double>();
+        //each list holds the average of F and M life expectancy per state for that quartile 
+        ArrayList<Double> Q1 = new ArrayList<Double>();
+        ArrayList<Double> Q2 = new ArrayList<Double>();
+        ArrayList<Double> Q3 = new ArrayList<Double>();
+        ArrayList<Double> Q4 = new ArrayList<Double>();
         
-//         //loops through all of the rows (states)
-//         while(sc.hasNextLine()){
-//             //DONT FORGET TO PARSE THE DOUBLES
-//             //takes the value at the index of q1_M and adds to q1_F, then divides by 2 and adds to the arrayList Q1
-//             //takes the value at the index of q2_M and adds to q2_F, then divides by 2 and adds to the arrayList Q2
-//             //takes the value at the index of q3_M and adds to q3_F, then divides by 2 and adds to the arrayList Q3
-//             //takes the value at the index of q4_M and adds to q4_F, then divides by 2 and adds to the arrayList Q4
+        //loops through all of the rows (states)
+        while(sc.hasNextLine()){
+            ArrayList<String> line = new ArrayList<>(Arrays.asList(sc.nextLine().split(",")));
+            //DONT FORGET TO PARSE THE DOUBLES
+            double Q1Sum=0;
+            double Q1Average =0;
+            Q1Sum += Double.parseDouble(line.get(q1MIndex));
+            Q1Sum += Double.parseDouble(line.get(q1FIndex));
+            Q1Average = Q1Sum/2;
+            Q1.add(Q1Average);
 
-//         }
+            double Q2Sum=0;
+            double Q2Average = 0;
+            Q2Sum += Double.parseDouble(line.get(q2MIndex));
+            Q2Sum += Double.parseDouble(line.get(q2FIndex));
+            Q2Average = Q2Sum/2;
+            Q2.add(Q2Average);
+
+            double Q3Sum=0;
+            double Q3Average = 0;
+            Q3Sum += Double.parseDouble(line.get(q3MIndex));
+            Q3Sum += Double.parseDouble(line.get(q3FIndex));
+            Q3Average = Q3Sum/2;
+            Q3.add(Q3Average);
+
+            double Q4Sum=0;
+            double Q4Average = 0;
+            Q4Sum += Double.parseDouble(line.get(q4MIndex));
+            Q4Sum += Double.parseDouble(line.get(q4FIndex));
+            Q4Average = Q4Sum/2;
+            Q4.add(Q4Average);
+
+            //takes the value at the index of q1_M and adds to q1_F, then divides by 2 and adds to the arrayList Q1
+            //takes the value at the index of q2_M and adds to q2_F, then divides by 2 and adds to the arrayList Q2
+            //takes the value at the index of q3_M and adds to q3_F, then divides by 2 and adds to the arrayList Q3
+            //takes the value at the index of q4_M and adds to q4_F, then divides by 2 and adds to the arrayList Q4
+
+        }
         
-//         //averagint the life expectancies in the list and putting it into the life expectancy array
-//         //QUARTILE 1
-//         double sumQ1 = 0;
-//         for (int i =0; i<Q1.size(); i++){
-//             sumQ1 += Q1.get(i);
-//         }
-//         double averageQ1 = sumQ1/Q1.size();
-//         //adding to the array
-//         quartileAverages[0] = averageQ1;
+        //averaging the life expectancies in the list and putting it into the life expectancy array
+        //QUARTILE 1
+        double sumQ1 = 0;
+        for (int i =0; i<Q1.size(); i++){
+            sumQ1 += Q1.get(i);
+        }
+        double averageQ1 = sumQ1/Q1.size();
+        //adding to the array
+        quartileAverages[0] = averageQ1;
 
-//         //QUARTILE 2
-//         double sumQ2 = 0;
-//         for (int i =0; i<Q2.size(); i++){
-//             sumQ2 += Q2.get(i);
-//         }
-//         double averageQ2 = sumQ2/Q2.size();
-//         //adding to the array
-//         quartileAverages[1] = averageQ2;
+        //QUARTILE 2
+        double sumQ2 = 0;
+        for (int i =0; i<Q2.size(); i++){
+            sumQ2 += Q2.get(i);
+        }
+        double averageQ2 = sumQ2/Q2.size();
+        //adding to the array
+        quartileAverages[1] = averageQ2;
       
-//         //QUARTILE 3
-//         double sumQ3 = 0;
-//         for (int i =0; i<Q3.size(); i++){
-//             sumQ3 += Q3.get(i);
-//         }
-//         double averageQ3 = sumQ3/Q3.size();
-//         //adding to the array
-//         quartileAverages[2] = averageQ3;
+        //QUARTILE 3
+        double sumQ3 = 0;
+        for (int i =0; i<Q3.size(); i++){
+            sumQ3 += Q3.get(i);
+        }
+        double averageQ3 = sumQ3/Q3.size();
+        //adding to the array
+        quartileAverages[2] = averageQ3;
         
-//         //QUARTILE 4
-//         double sumQ4 = 0;
-//         for (int i =0; i<Q4.size(); i++){
-//             sumQ4 += Q4.get(i);
-//         }
-//         double averageQ4 = sumQ4/Q4.size();
-//         //adding to the array
-//         quartileAverages[3] = averageQ4;
+        //QUARTILE 4
+        double sumQ4 = 0;
+        for (int i =0; i<Q4.size(); i++){
+            sumQ4 += Q4.get(i);
+        }
+        double averageQ4 = sumQ4/Q4.size();
+        //adding to the array
+        quartileAverages[3] = averageQ4;
 
-//         indexQuartileMax = indexOf(quartiles.max());
-//         //everything that i want to be printed out 
-//         System.out.println("The First Quartile Average Life Expectancy is " + quartiles[0]);
-//         System.out.println("The Second Quartile Average Life Expectancy is " + quartiles[1]);
-//         System.out.println("The Third Quartile Average Life Expectancy is " + quartiles[2]);
-//         System.out.println("The Fourth Quartile Average Life Expectancy is " + quartiles[3]);
-//         System.out.println("The winning average life expectancy is " + quartiles.max() " which is Quartile " + (indexOf(quartiles.max())+1));
-//         //is .max() not a statement that i can use?
-//         //is indexOf not a function I can use?
-//     }
+        indexQuartileMax = quartileAverages.indexOf(max(quartileAverages));
+        //everything that i want to be printed out 
+        System.out.println("The First Quartile Average Life Expectancy is " + quartileAverages.get(0));
+        System.out.println("The Second Quartile Average Life Expectancy is " + quartileAverages.get(1));
+        System.out.println("The Third Quartile Average Life Expectancy is " + quartileAverages.get(2));
+        System.out.println("The Fourth Quartile Average Life Expectancy is " + quartileAverages.get(3));
+        System.out.println("The winning average life expectancy is " + max(quartileAverages) + " which is Quartile " + (quartileAverages.indexOf(max(quartileAverages)+1)));
+        //is .max() not a statement that i can use?
+        //is indexOf not a function I can use?
+    }
 
 //Assuming all states are either in the redstates data file or the bluestates data file 
     public static void returnWinningParty (String lifeExpectancyPathname, String pathnameBlue) throws FileNotFoundException{
